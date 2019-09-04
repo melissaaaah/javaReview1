70ba36445ea2e5054e0b60b42a29615aa138ef6c
mhuynh76 <mhuynh76@OVDIMC10-315V.coast.cccd.adx>
2019-09-04 12:57:51 -0700

    heads

--- In-Class-01-Java-Review/src/review1/JavaReview1.java
+++ In-Class-01-Java-Review/src/review1/JavaReview1.java
@@ -1,16 +1,16 @@
 package review1;
 
+import java.util.Scanner;
+
 public class JavaReview1
 {
 
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
-        System.out.println("hi");
+        System.out.println("How many times should the coin be flipped?");
         
-        System.out.println("bye");
-        
-        
+       
         
     }
 
