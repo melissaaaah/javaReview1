f2fa7717f32946b4d0aa1718255574b8a55c8f3c
mhuynh76 <mhuynh76@OVDIMC10-315V.coast.cccd.adx>
2019-09-04 12:46:25 -0700

    changed

--- In-Class-01-Java-Review/src/review1/JavaReview1.java
+++ In-Class-01-Java-Review/src/review1/JavaReview1.java
@@ -6,7 +6,11 @@ public class JavaReview1
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
-        System.out.println();
+        System.out.println("hi");
+        
+        System.out.println("bye");
+        
+        
     }
 
 }
