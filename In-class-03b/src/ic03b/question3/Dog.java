eeb569d931430ebe0139c9d28d417e4a6da39c2a
mhuynh76 <mhuynh76@OVDIMC10-352V.coast.cccd.adx>
2019-09-16 12:50:56 -0700

    in class b

---
+++ In-class-03b/src/ic03b/question3/Dog.java
@@ -0,0 +1,21 @@
+package ic03b.question3;
+
+public class Dog implements Speakable
+{
+   private String name;
+
+   public Dog(String name)
+   {
+      this.name = name;
+   }
+
+   public void speak()
+   {
+     System.out.println("Woof! Woof!");
+   }
+
+   public String toString()
+   {
+      return "Dog:  " + name;
+   }
+}
---
+++ In-class-03b/src/ic03b/question3/Speakable.java
@@ -0,0 +1,6 @@
+package ic03b.question3;
+
+public interface Speakable
+{
+    void speak();
+}
