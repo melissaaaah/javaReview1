20855893dbede7dfdb795c702e0483b3cbe02291
mhuynh76 <mhuynh76@OVDIMC10-134V.coast.cccd.adx>
2019-09-25 13:39:38 -0700

    in class b

--- In-Class-05A-IO/src/occ/cs272/ic05/Exeptions01.java
+++ In-Class-05A-IO/src/occ/cs272/ic05/Exeptions01.java
@@ -16,17 +16,54 @@ public class Exeptions01
     {
         try (Scanner cin = new Scanner(System.in))
         {
-            System.out.println("int[] a = {5, 6, 7};");
-            System.out.print("What element do you want? ");
-            
+
             int [] ar = {5, 6, 7};
             
-            String indexStr = cin.next();
-            int index = Integer.parseInt(indexStr);
-    //        int index = System.in.read();
-    
-            int value = ar[index];
-            System.out.printf("ar[%d] = %d%n", index, value);
+            boolean ok = false;
+            while (!ok)
+            {
+                System.out.println("int[] a = {5, 6, 7};");
+                System.out.print("What element do you want? ");
+            try
+            {
+                ok = true;
+                String indexStr = cin.next();
+                int index = Integer.parseInt(indexStr);
+                //int index = System.in.read();
+   
+                int value = ar[index];
+                System.out.printf("ar[%d] = %d%n", index, value);
+            }
+            /*
+            catch (NumberFormatException e)
+            {
+                // TODO Auto-generated catch block
+               // e.printStackTrace();
+                System.err.println("Please use an integer input.");
+            }
+            catch(ArrayIndexOutOfBoundsException e)
+            {
+                System.err.println("Please use index between 0.." + (ar.length-1));
+            }
+            */
+            catch (Exception e)
+            {
+                if(e instanceof NumberFormatException)
+                {
+                    System.err.println("Please use an integer input.");
+                }
+                else if(e instanceof ArrayIndexOutOfBoundsException)
+                {
+                    System.err.println("Please use index between 0.." + (ar.length-1));
+                }
+                else
+                {
+                    System.err.println("WRONG");
+                    e.printStackTrace();
+                }
+                ok = false;
+            }
+            }
         }
     }
 }
